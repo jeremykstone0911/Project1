f58a1adef75c91a144ebb3f3bce813497a5b18b5
Valerie Hill <itsvhill@gmail.com>
2020-04-22 16:01:59 -0500

    onclick

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,62 @@
 $(document).ready(function () {
   $(".datepicker").datepicker();
+
+  $("#submit-button").on("click", function () {
+    // Create API variables
+    var userDate = $("#user-date").val();
+    console.log(userDate);
+
+    if (userDate != "") {
+      $.ajax({
+        url: "http://api.eventful.com/json/events/search?app_key=B3cccB54Cfm7csbL&category=music&date=past" +
+          userDate,
+        type: "GET",
+        dataType: "jsonp",
+        dateFormat: "yyyy/mm/dd",
+
+        success: function (response) {
+          console.log(response);
+
+          var display = displayEventInfo(response);
+          $(".wind").text("Wind Speed: " + response.wind.speed);
+          $("#current-day").val("");
+        },
+      });
+    } else {
+      $("#error").html("Field cannot be blank");
+    }
+  });
 });
 
-function show_alert() {
-  var oArgs = {
-    app_key: "YOUR APP KEY",
+//function show_alert() {
+//  var oArgs = {
+//    app_key: "YOUR APP KEY",
 
-    id: "20218701",
+//    id: "20218701",
 
-    page_size: 25,
-  };
+//    page_size: 25,
+//  };
 
-  EVDB.API.call("/events/get", oArgs, function (oData) {
-    // Note: this relies on the custom toString() methods below
-  });
-}
+//  EVDB.API.call("/events/get", oArgs, function (oData) {
+// Note: this relies on the custom toString() methods below
+//  });
+//}
 
-function show_alert2() {
-  var oArgs = {
-    app_key: "YOUR APP KEY",
+//function show_alert2() {
+//  var oArgs = {
+//    app_key: "YOUR APP KEY",
 
-    q: "music",
+//    q: "music",
 
-    where: "San Diego",
+//    where: "San Diego",
 
-    date: "2013061000-2015062000",
+//    date: "2013061000-2015062000",
 
-    page_size: 5,
+//  page_size: 5,
 
-    sort_order: "popularity",
-  };
+//    sort_order: "popularity",
+//  };
 
-  EVDB.API.call("/events/search", oArgs, function (oData) {
-    // Note: this relies on the custom toString() methods below
-  });
-}
+//  EVDB.API.call("/events/search", oArgs, function (oData) {
+// Note: this relies on the custom toString() methods below
+// });}
