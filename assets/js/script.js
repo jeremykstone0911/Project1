c3d3218a8b5ea2f654044f11c98374b6a6ddda50
Valerie Hill <itsvhill@gmail.com>
2020-04-24 19:25:37 -0500

    and fixing still

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,17 +12,24 @@ $(document).ready(function () {
         url: "https://api.eventful.com/json/events/search?app_key=B3cccB54Cfm7csbL&category=music&include=popularity&location=United+States&page_size=4&date=" + formattedDate,
         method: "GET",
         dataType: "jsonp",
-      }).then(function(response) {
+      })
+      
+      function displayWeatherInfo(response) {
           console.log(response);
         
-          $("#artist").text("Artist: " + response.events.event.title);
-          $("#artist").html("Location: " + response.events.event.city_name + response.events.event.region_name);
-          $("#artist").html("Venue: " + response.events.event.venue_name);
+          var artist = $("#artist").text("Artist: " + response.events.event.title);
+          var location =$("#artist").html("Location: " + response.events.event.city_name + response.events.event.region_name);
+          var venue = $("#artist").html("Venue: " + response.events.event.venue_name);
 
-          console.log("Artist: " + response.events.event.title);
-          console.log("Location: " + response.events.event.city_name + response.events.event.region_name);
-          console.log("Venue: " + response.events.event.venue_name);
+          console.log(artist);
+          console.log(location);
+          console.log(venue);
 
+          $(".collection").append(
+            artist,
+            location,
+            venue,
+          );
 
           $(".title-two").html("Artist: " + response.events.event.title);
           $("#location-two").html("Location: " + response.events.event.city_name + response.events.event.region_name);
@@ -35,6 +42,7 @@ $(document).ready(function () {
           $(".title-four").html("Artist: " + response.events.event.title);
           $("#location-four").html("Location: " + response.events.event.city_name + response.events.event.region_name);
           $("#venue-four").html("Venue: " + response.events.event.venue_name);
-      })  
+
+      }  
   })
 })
